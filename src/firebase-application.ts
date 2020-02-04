1b0d0183ca2a13cbf21ba3a90d538c3d381faac8
Oleg V. Kuzmin <inpassor@yandex.com>
2020-02-04 20:15:41 +0300

    fix Server

--- src/firebase-application.ts
+++ src/firebase-application.ts
@@ -1,10 +1,29 @@
+import { apps, app, firestore, initializeApp } from 'firebase-admin';
 import { RuntimeOptions, VALID_MEMORY_OPTIONS, HttpsFunction, runWith } from 'firebase-functions';
-import { ServerConfig, isServerConfig } from '@inpassor/node-server';
-import { Server } from './server';
+import { Server as NodeServer, ServerConfig, isServerConfig } from '@inpassor/node-server';
+
+export type Server = NodeServer & {
+    firebaseApp: app.App;
+    firestore: firestore.Firestore;
+};
 
 const defaultRuntimeOptions: RuntimeOptions = {
     timeoutSeconds: 10,
     memory: VALID_MEMORY_OPTIONS['128MB'],
+};
+
+const createServer = (config: ServerConfig): Server => {
+    const server = new NodeServer(config) as Server;
+    if (!apps.length) {
+        server.firebaseApp = initializeApp({
+            projectId: process.env.GCLOUD_PROJECT,
+        });
+        server.firestore = this.firebaseApp.firestore();
+    } else {
+        server.firebaseApp = app();
+        server.firestore = firestore(this.firebaseApp);
+    }
+    return server;
 };
 
 export const firebaseApplication = (
@@ -13,14 +32,14 @@ export const firebaseApplication = (
 ): HttpsFunction => {
     const onRequest = runWith(runtimeOptions).https.onRequest;
     if (isServerConfig(getConfig)) {
-        const server = new Server(getConfig);
+        const server = createServer(getConfig);
         return onRequest(server.handle.bind(server));
     }
     return onRequest(async (request, response) => {
         await new Promise((resolve, reject) => {
             Promise.resolve(getConfig).then(
                 (config): void => {
-                    const server = new Server(config);
+                    const server = createServer(config);
                     resolve(server.handle.call(server, request, response));
                 },
                 error => reject(error),
